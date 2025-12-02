622215f1221d9c94e6b0cb18c3d29cb6ef230e24
Woad Stoat <woadstoat@gmail.com>
2025-12-02 09:18:08 +0000

    !U Updated Build

--- obj/Debug/net8.0/Engine.AssemblyInfo.cs
+++ obj/Debug/net8.0/Engine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e5cbfce85c24bdf47158686e7554448b7c4fd88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272f005a96af1611694efd20a00a03ac6557ac0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
