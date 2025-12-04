8a294f8f6dd63ac6b19bcb56d61ed53275c377d5
Woad Stoat <woadstoat@gmail.com>
2025-12-04 10:11:44 +0000

    !U Added more tags

--- Engine/ECS/Systems/TagComponent.cs
+++ Engine/ECS/Systems/TagComponent.cs
@@ -6,10 +6,13 @@ public enum Tags
 {
     None = 0,
     Player = 1 << 0,
-    Player2 = 1 << 2,
+    Player2 = 1 << 1,
+    Enemy = 1 << 2,
     Ball = 1 << 3,
-    UI = 1 << 4
-    
+    UI = 1 << 4,
+    Powerup = 1 << 5,
+    Brick = 1 << 6,
+    Paddle = 1 << 7
 }
 
 namespace WoadEngine.ECS
